d572091aace7468cde27b73e39a69944434062bf
Jamey Hicks <jamey.hicks@gmail.com>
2015-04-16 12:21:40 -0400

    added constraints

--- projects/fmcomms2_df/zed/system_project.tcl
+++ projects/fmcomms2_df/zed/system_project.tcl
@@ -6,7 +6,8 @@ adi_project_files fmcomms2_i2c_zed [list \
   "system_top.v" \
   "$ad_hdl_dir/projects/fmcomms2/zed/system_constr.xdc"\
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
-  "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc" ]
+  "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc" \
+  "df_constraints.xdc" ]
 
 adi_project_run fmcomms2_i2c_zed
 
